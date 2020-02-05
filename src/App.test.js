2c0278b3203c9f552fd6a7441f6e889374dde1ca
Philippe Métais <phi.metais@gmail.com>
2020-02-06 00:11:44 +0100

    :green_heart: Fixing CI Build.

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+
+const store = configureStore(window.__INITIAL_STATE__);
 
 describe('rendering', () => {
   it('renders App', () => {
     expect.hasAssertions();
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    const linkElement = getByText(/transaction pool/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
